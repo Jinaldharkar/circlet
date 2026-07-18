0bbc7d9cb73323e1226d7fb7fc80f00f65182a05
Jinaldharkar <jinidharkar123@gmail.com>
2026-07-18 16:09:47 +0530

    Add files via upload

---
+++ circuit2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+ {
+   
+   int i,j,num=11;
+   
+   for(i=11; i<=14; i++)
+   {
+       for(j=11; j<=i; j++)
+       {
+           printf("%d ",num);
+           num++;
+       }
+       
+         printf("\n");
+         
+    }  
+    
+   }
+   
